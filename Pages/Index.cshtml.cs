4386d155ae5925e5362245448b5b807ec412238c
Klaas Boshuisen <klaas@boshuisen.net>
2020-04-14 22:49:01 +0200

    cookies out

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -25,45 +25,28 @@ namespace cachetest.Pages
         }
 
 
-        [ResponseCache(VaryByHeader = "User-Agent", Duration = 30)]
+      
         public void OnGet()
         {
-            CookieOptions cookieOptions = new CookieOptions();
+            // CookieOptions cookieOptions = new CookieOptions();
 
-            if(!HttpContext.Request.Cookies.ContainsKey("first_request"))
-            {
+            // if(!HttpContext.Request.Cookies.ContainsKey("first_request"))
+            // {
 
-                cookieOptions.Expires = new DateTimeOffset(DateTime.Now.AddDays(7));
-                HttpContext.Response.Cookies.Append("first_request", DateTime.Now.ToString(),cookieOptions);
+            //     cookieOptions.Expires = new DateTimeOffset(DateTime.Now.AddDays(7));
+            //     HttpContext.Response.Cookies.Append("first_request", DateTime.Now.ToString(),cookieOptions);
                 
-                WelcomeMessage = "Welcome, new visitor!";
-            } else {
-                DateTime firstRequest = DateTime.Parse(HttpContext.Request.Cookies["first_request"]);
-                WelcomeMessage = "Welcome back, user!" ;
-                FirstVisitDate = "your first visit was on:" + firstRequest.ToString();
-            }
+            //     WelcomeMessage = "Welcome, new visitor!";
+            // } else {
+            //     DateTime firstRequest = DateTime.Parse(HttpContext.Request.Cookies["first_request"]);
+            //     WelcomeMessage = "Welcome back, user!" ;
+            //     FirstVisitDate = "your first visit was on:" + firstRequest.ToString();
+            // }
             
 
         }
 
         
-        /// <summary>  
-        /// set the cookie  
-        /// </summary>  
-        /// <param name="key">key (unique indentifier)</param>  
-        /// <param name="value">value to store in cookie object</param>  
-        /// <param name="expireTime">expiration time</param>  
-        public  void Set(string key, string value, int? expireTime)  
-        {  
-        CookieOptions option = new CookieOptions();  
-
-        if (expireTime.HasValue)  
-                option.Expires = DateTime.Now.AddMinutes(expireTime.Value);  
-        else  
-                option.Expires = DateTime.Now.AddMilliseconds(10);  
-        
-        Response.Cookies.Append(key, value, option);  
-        }   
-
+       
     }
 }
