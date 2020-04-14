34f8832f0d880b05335cb5edda7fa34e3a572c6b
Klaas Boshuisen <klaas@boshuisen.net>
2020-04-14 18:29:13 +0200

    added resquest response logging

--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using cachetest.Middleware;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -39,7 +40,7 @@ namespace cachetest
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 //app.UseHsts();
             }
-
+            app.UseRequestResponseLogging();
             //app.UseHttpsRedirection();
             app.UseStaticFiles();
 
